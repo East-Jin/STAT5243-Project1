f0bc09d1609e3d3a53753781a6012b7e835da5b5
Xiang Li <xl3548@columbia.edu>
2026-02-12 01:35:16 -0500

    Market Data

---
+++ Market_Data.py
@@ -0,0 +1,20 @@
+import databento as db
+import pandas as pd
+
+company = pd.read_csv('top20_sp500_tech_companies.csv')
+
+
+API_KEY = "API_Key" # Enter your API key
+
+client = db.Historical(API_KEY)
+
+data = client.timeseries.get_range(
+    dataset="XNAS.ITCH", # XNAS.ITCH = Nasdaq; XNYS.ITCH = NYSE
+    schema="ohlcv-1s",
+    symbols=["AAPL"], # Company name
+    start="2025-10-28T20:00:00Z",  # UTC Time
+    end="2025-10-28T23:00:00Z"
+)
+
+df = data.to_df()
+print(df.head())
