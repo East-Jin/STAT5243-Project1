26e6e2a19efbfc7616d823e69eb04001841db2ac
Xiang Li <xl3548@columbia.edu>
2026-02-13 17:34:26 -0500

    code to gather the marketing data

--- Market_Data.py
+++ Market_Data.py
@@ -2,7 +2,7 @@ import databento as db
 import pandas as pd
 
 company = pd.read_csv('top20_sp500_tech_companies.csv')
-company['识别号'] = company['交易所'].apply(lambda x: 'XNAS.ITCH' if x.upper() == 'NASDAQ' else 'XNYS.ITCH') # XNAS.ITCH = Nasdaq; XNYS.ITCH = NYSE
+company['识别号'] = company['交易所'].apply(lambda x: 'XNAS.ITCH' if x.upper() == 'NASDAQ' else 'XNYS.PILLAR') # XNAS.ITCH = Nasdaq; XNYS.PILLAR = NYSE
 
 header_mapping = {
     '排名': 'Rank',
@@ -13,17 +13,38 @@ header_mapping = {
 }
 company = company.rename(columns=header_mapping)
 
-API_KEY = "API_Key" # Enter your API key
+exchange = "ENTER HERE" # XNAS.ITCH = Nasdaq; XNYS.PILLAR = NYSE
+ticker = "ENTER HERE" # ticker name
+callname = "ENTER HERE"
+utc_time_str = "ENTER HERE" # earning call start time in UTC
 
+utc_time = pd.Timestamp(utc_time_str) # change string to timedelta
+start_utc = utc_time - pd.Timedelta(hours=1)
+end_utc = utc_time + pd.Timedelta(hours=2)
+
+API_KEY = "ENTER HERE" # enter your API key
 client = db.Historical(API_KEY)
 
-data = client.timeseries.get_range(
-    dataset="XNAS.ITCH", # XNAS.ITCH = Nasdaq; XNYS.ITCH = NYSE
+cost = client.metadata.get_cost( # check the cost (we only have $125 in total)
+    dataset=exchange,
     schema="ohlcv-1s",
-    symbols=["AAPL"], # Company name
-    start="2025-10-28T20:00:00Z",  # UTC Time
-    end="2025-10-28T23:00:00Z"
+    symbols=[ticker], # Company name
+    start= start_utc,  # UTC Time
+    end= end_utc
 )
+print("The cost will be",cost) # after we find it's coverable, continue the next step
 
+data = client.timeseries.get_range(
+    dataset=exchange,
+    schema="ohlcv-1s",
+    symbols=[ticker], # Company name
+    start=start_utc,  # UTC Time
+    end=end_utc
+)
 df = data.to_df()
 print(df.head())
+
+# filename = f"{ticker}_{callname}.parquet"
+# df.to_parquet(filename)
+filename2 = f"{ticker}_{callname}.csv" # save the file
+df.to_csv(filename2)
